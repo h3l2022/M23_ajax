2d42bc13cc3d8a9a1a098f6236734f66d40cc379
Lucas Mario <lucasmariocontato@gmail.com>
2024-04-22 14:28:27 -0300

    finalização do modulo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('#name');
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#nome');
     const usernameElement = document.querySelector('#username');
     const avatarElement = document.querySelector('#avatar');
     const reposElement = document.querySelector('#repos');
@@ -7,17 +7,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
 
-    fetch('https://api.github.com/user/h3l2022')
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(json) {
-        nameElement.innerText = (json.name);
-        usernameElement.innerText = (json.login);
-        avatarElement.src = (json.avatar_url);
-        followingElement.innerText = (json.following);
-        followersElement.innerText = (json.followers);
-        repos.innerText = (json.public_repos);
-        linkElement.href = (json.html_url);
-    })
-})
+    try {
+        fetch('https://api.github.com/users/h3l2022')
+            .then(function(res) {
+                if (!res.ok) {
+                    throw new Error('A resposta da rede não foi boa');
+                }
+                return res.json();
+            })
+            .then(function(json) {
+                nameElement.innerText = json.name;
+                usernameElement.innerText = json.login;
+                avatarElement.src = json.avatar_url;
+                followersElement.innerText = json.followers;
+                followingElement.innerText = json.following;
+                reposElement.innerText = json.public_repos;
+                linkElement.href = json.html_url;
+            })
+            .catch(function(error) {
+                console.error('Houve um problema com a operação de busca:', error);
+            });
+    } catch (error) {
+        console.error('Ocorreu um erro durante a operação de busca:', error);
+    }
+});
